877f00b0e26d340c361ee42db0f6a086471c43b9
habeebhassan <bidehassan@gmail.com>
2021-05-13 13:30:52 -0400

    set Django secret keys and debug variables

--- zarawa_site/settings.py
+++ zarawa_site/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = '6=k3-zhnop9aoi3+hi3(0e3*4r64ibk4v51qp^6f)%r^!72h0i'
 
-SECRET_KEY = oos.environ.get('DJANGO_SECRET_KEY', 'ee4oswu+ycp8oyhjr4pcv3+wq0fwtpww%$qg0fc$uz_f3mxh4g')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'ee4oswu+ycp8oyhjr4pcv3+wq0fwtpww%$qg0fc$uz_f3mxh4g')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = False
